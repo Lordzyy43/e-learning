9ef01a88fba39eccfc50a355aa1321cc1c529673
yogie <lordz0990@gmail.com>
2025-10-25 09:10:53 +0700

    .

--- admin/admin_check.php
+++ admin/admin_check.php
@@ -1,10 +1,9 @@
 <?php
-<<<<<<< HEAD
 session_start();
 
 // kalau belum login sama sekali
 if (!isset($_SESSION['role'])) {
-=======
+
 // check_admin.php
 
 if (session_status() == PHP_SESSION_NONE) {
@@ -13,18 +12,15 @@ if (session_status() == PHP_SESSION_NONE) {
 
 // Cek apakah user sudah login
 if (!isset($_SESSION['user_id'])) {
->>>>>>> 27b21ad70aca052e6dbc824a33fb8440cfdef150
     header("Location: ../login.php");
     exit;
 }
 
-<<<<<<< HEAD
 // kalau yang login bukan admin
-=======
 // Cek apakah user bukan admin
->>>>>>> 27b21ad70aca052e6dbc824a33fb8440cfdef150
 if ($_SESSION['role'] !== 'admin') {
     header("Location: ../index.php");
     exit;
+}
 }
 ?>
